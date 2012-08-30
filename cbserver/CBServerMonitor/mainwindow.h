96f80a5cf885d184a48bccb0a1a0c20bfcf8578c
Raiden Awkward <raiden.ht@gmail.com>
2012-08-30 21:38:30 +0800

    double click on dishes table item supported

--- cbserver/CBServerMonitor/mainwindow.h
+++ cbserver/CBServerMonitor/mainwindow.h
@@ -48,6 +48,7 @@ protected slots:
 private slots:
     void on_buttonAdd_clicked();
     void on_buttonEdit_clicked();
+    void on_tableWidgetDish_doubleClicked(const QModelIndex &index);
     void on_buttonRefresh_clicked();
     void on_tableWidget_cellDoubleClicked(int, int);
     void on_buttonRemove_clicked();
